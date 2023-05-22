4c104aaedc3ea9940a8f331d0b0b55770d0d73c9
Igor Šešlak <95866956+himym413@users.noreply.github.com>
2023-05-22 02:03:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-alert(1);
+
 const steps = document.querySelectorAll(".main__modal");
 const stepNumbers = document.querySelectorAll(".header__steps__step");
 const btnsContainer = document.querySelector(".main__buttons");
@@ -169,7 +169,6 @@ const validateForm = () => {
       errorMessage.classList.add("hidden");
     }, 3000);
   }
-alert(1);
   return stop;
 };
 
